1f48be155d59f038a864238b0d3a6d82374858d1
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-08-06 14:20:44 +0200

    Decoupled admin page to separate view

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,13 +1,62 @@
 <template>
-    <div>
-
+    <div class="add-posts">
+        <form @submit.prevent="addNewPost">
+            <div class="form-group">
+                <label for="title">Title:</label>
+                <input
+                    v-model="newPost.title"
+                    type="text"
+                    id="title"
+                    placeholder="Enter post title"
+                />
+            </div>
+            <div class="form-group">
+                <label for="content">Content:</label>
+                <textarea
+                    v-model="newPost.content"
+                    id="content"
+                    placeholder="Enter post content"
+                ></textarea>
+            </div>
+            <div class="form-group">
+                <label for="photo">Photo URL:</label>
+                <input
+                    v-model="newPost.image"
+                    type="text"
+                    id="photo"
+                    placeholder="Enter photo URL"
+                />
+            </div>
+            <button type="submit">Add Post</button>
+        </form>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted, computed } from 'vue'
+import { usePostStore } from '@/stores/posts'
 
+const postStore = usePostStore()
+
+const newPost = ref({
+    title: '',
+    content: '',
+    image: '',
+})
+
+const addNewPost = async () => {
+    if (newPost.value.title && newPost.value.content && newPost.value.image) {
+        console.log(newPost.value)
+        await postStore.addPost(newPost.value)
+        newPost.value = {
+            title: '',
+            content: '',
+            image: '',
+        }
+    } else {
+        alert('Please fill out all fields before submitting.')
+    }
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,5 @@
 <template>
     <main>
-        <div class="add-posts">
-            <form @submit.prevent="addNewPost">
-                <div class="form-group">
-                    <label for="title">Title:</label>
-                    <input
-                        v-model="newPost.title"
-                        type="text"
-                        id="title"
-                        placeholder="Enter post title"
-                    />
-                </div>
-                <div class="form-group">
-                    <label for="content">Content:</label>
-                    <textarea
-                        v-model="newPost.content"
-                        id="content"
-                        placeholder="Enter post content"
-                    ></textarea>
-                </div>
-                <div class="form-group">
-                    <label for="photo">Photo URL:</label>
-                    <input
-                        v-model="newPost.image"
-                        type="text"
-                        id="photo"
-                        placeholder="Enter photo URL"
-                    />
-                </div>
-                <button type="submit">Add Post</button>
-            </form>
-        </div>
         <div v-for="post in posts" :key="post.id">
             <h1>{{ post.details.title }}</h1>
             <p>{{ post.details.content }}</p>
@@ -49,24 +18,4 @@ onMounted(async () => {
     await postStore.fetchPosts()
 })
 const posts = computed(() => postStore.posts)
-
-const newPost = ref({
-    title: '',
-    content: '',
-    image: '',
-})
-
-const addNewPost = async () => {
-    if (newPost.value.title && newPost.value.content && newPost.value.image) {
-        console.log(newPost.value)
-        await postStore.addPost(newPost.value)
-        newPost.value = {
-            title: '',
-            content: '',
-            image: '',
-        }
-    } else {
-        alert('Please fill out all fields before submitting.')
-    }
-}
 </script>
