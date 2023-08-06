74f8c71ab3c2c22b1a29b1854a758b4b08a03904
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-08-06 18:24:28 +0200

    Started styling the main page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <template>
     <main>
-        <div v-for="post in posts" :key="post.id">
-            <h1>{{ post.details.title }}</h1>
-            <p>{{ post.details.content }}</p>
-            <img :src="post.details.image" alt="" />
+        <div class="post_gallery">
+            <div class="post" v-for="post in posts" :key="post.id">
+                <img :src="post.details.image" alt="" />
+                <div class="text">
+                    <h1>{{ post.details.title }}</h1>
+                    <p>{{ post.details.content }}</p>
+                </div>
+            </div>
         </div>
     </main>
 </template>
@@ -19,3 +23,41 @@ onMounted(async () => {
 })
 const posts = computed(() => postStore.posts)
 </script>
+
+<style scoped lang="scss">
+.post_gallery {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    margin-top: 1rem;
+
+    .post {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: flex-start;
+        width: 100%;
+        height: 100vh;
+        margin: 1rem 0;
+        padding: 1rem;
+
+        img {
+            width: 30%;
+            height: auto;
+            object-fit: cover;
+            border-radius: 5px;
+        }
+
+        .text {
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-start;
+            align-items: flex-start;
+            width: 70%;
+            margin-left: 1rem;
+        }
+    }
+}
+</style>
