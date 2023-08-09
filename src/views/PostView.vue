1bed853ecd16d14dee7f621934cf1263eb856f1f
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-08-09 22:18:46 +0200

    Added styling and links to PostView

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -12,9 +12,13 @@
                 {{ author }}
             </h3>
             <div class="links">
-                <router-link to="'/'">Back to Blog</router-link>
+                <router-link to="'/'">
+                    <span class="arrow-icon" v-html="leftArrowIcon" />Back to
+                    Blog</router-link
+                >
                 <a v-if="nextPost" :href="'/post/' + nextPost.id">
                     {{ nextPost.title }}
+                    <span class="arrow-icon" v-html="rightArrowIcon" />
                 </a>
             </div>
         </div>
@@ -60,6 +64,18 @@ const nextPost = computed(() => {
     return postStore.posts[currentPostIndex + 1]
 })
 
+const leftArrowIcon = `
+<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+</svg>
+`
+
+const rightArrowIcon = `
+<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+</svg>
+`
+
 // Fetch post data when component is mounted
 onMounted(async () => {
     await postStore.fetchPosts()
@@ -89,10 +105,14 @@ onMounted(async () => {
     .links {
         display: flex;
         flex-direction: row;
+        justify-content: space-between;
         align-items: center;
         margin: 1rem;
+        width: 100%;
+        max-width: 800px;
 
         a {
+            display: block;
             color: var(--dark-gray);
             font-size: 1.5rem;
             font-weight: 400;
@@ -120,6 +140,13 @@ onMounted(async () => {
     height: 40px;
     animation: spin 1s linear infinite;
     margin-bottom: 1rem;
+}
+
+.arrow-icon {
+    width: 20px;
+    height: 20px;
+    fill: var(--dark-gray);
+    margin: 0 0.5rem;
 }
 
 @keyframes spin {
